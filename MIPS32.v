bd8e17836b55cd5784e279f573091ec668b7f1bf
Sat <satjpatel007@gmail.com>
2020-08-11 17:56:33 +0530

    Instruction Fetch and Instruction Decode stages done

--- MIPS32.v
+++ MIPS32.v
@@ -22,6 +22,8 @@ parameter ADD = 6'b000000 , SUB = 6'b000001, AND = 6'b000010,
 parameter RR_ALU = 3'b000, RM_ALU = 3'b001, LOAD = 3'b010, 
 		STORE = 3'b011, BRANCH = 3'b100, HALT = 3'b101 ; 
 		
+reg [2:0] ID_EX_type, EX_MEM_type, MEM_WB_type ; 
+		
 reg HALTED ; 
 //Set after HLT instruction is completed - in WB stage 
 
@@ -30,14 +32,78 @@ reg TAKEN_BRANCH ;
 
 
 //Defining intermediate latches between Instruction Fetch stage 
-//and Instruction Decode 
+//and Instruction Decode stage 
 
 reg [31:0] PC, IF_ID_IR, IF_ID_NPC ; 
 //PC- Program Counter 
 //IR - Instruction Register 
 //NPC - Next Program Counter 
 
+//Defining intermediate latches between Instruction Decode stage and 
+//Execute stage 
+reg [31:0] ID_EX_IR, ID_EX_NPC, ID_EX_A, ID_EX_B, ID_EX_Imm ; 
+
+//Defining intermediate latches between Execute Stage and Memory Stage 
+reg [31:0] EX_MEM_IR, EX_MEM_ALUOut, EX_MEM_B ; 
+reg EX_MEM_cond ; 
+
+//Defining intermediate latches between Memory Stage and Write back stage 
+reg [31:0] MEM_WB_IR, MEM_WB_ALUOut, MEM_WB_LMD ; 
+
 //Instruction Fetch Stage 
+
+always @ ( posedge clk1 ) 
+
+	if ( HALTED == 0 ) 
+		begin 
+			if(((EX_MEM_IR[31:26] == BEQZ) && (EX_MEM_cond == 1)) || ((EX_MEM_IR[31:26] == BNEQZ) && (EX_MEM_cond == 0))) 
+				begin 
+					IF_ID_IR <= Mem[EX_MEM_ALUOut] ; 
+					TAKEN_BRANCH <= 1'b1 ; 
+					IF_ID_NPC <= EX_MEM_ALUOut + 1 ; 
+					PC <= EX_MEM_ALUOut + 1 ; 
+				end 
+		else 
+			begin 
+				IF_ID_IR <= Mem[PC] ; 
+				IF_ID_NPC <= PC + 1 ; 
+				PC <= PC + 1 ; 
+			end 
+		end 
+
+//Instruction Decode Stage 
+
+always @ ( posedge clk2 ) 
+
+	if(HALTED == 0 ) 
+		begin 
+			if(IF_ID_IR[25:21] == 5'b00000) 
+				ID_EX_A <= 0 ; 
+			else 
+				ID_EX_A <= RegBank[IF_ID_IR[25:21]] ; //"rs" 
+			
+			if(IF_ID_IR[20:16] == 5'b00000) 
+				ID_EX_B <= 0 ; 
+			else 
+				ID_EX_B <= RegBank[IF_ID_IR[20:16]] ; //"rt" 
+			
+			ID_EX_NPC <= IF_ID_NPC ; 
+			ID_EX_IR <= IF_ID_IR ; 
+			ID_EX_Imm <= { {16{IF_ID_IR[15]} } , {IF_ID_IR[15:0] }} ; 
+			
+		case (IF_ID_IR[31:26]) 
+			ADD, SUB, AND, OR, SLT, MUL:  ID_EX_type <= RR_ALU ; 
+			ADDI, SUBI, SLTI: 			  ID_EX_type <= RM_ALU ; 
+			LW: 						  ID_EX_type <= LOAD   ;
+			SW: 						  ID_EX_type <= STORE  ;	 
+			BNEQZ, BEQZ: 				  ID_EX_type <= BRANCH ;
+			HLT: 						  ID_EX_type <= HALT   ;
+			default: 					  ID_EX_type <= HALT   ;	
+		endcase 
+	end 
+
+
+
 
 endmodule 
 
