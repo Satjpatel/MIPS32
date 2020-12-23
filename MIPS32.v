169bb745cc4077babb9ac81b169f808003e1c57b
Sat <satjpatel007@gmail.com>
2020-12-23 22:26:49 +0530

    Code described in a better way

--- MIPS32.v
+++ MIPS32.v
@@ -1,14 +1,25 @@
 //MIPS32 Basic Implmentation
 //Engineer : Sat Patel
 
+//---------------------------------------------//
+//Acronyms used throught : 
+//IF - Instruction Fetch 
+//ID - Instruction Decode/Register Fetch 
+//EX - Execution/Effective Address Calculation 
+//MEM - Memory Access
+//WB - Write back 
+//---------------------------------------------//
+
+
 module MIPS32 ( clk1, clk2 ) ;
 
 //Using two phase clock, for avoiding clashes and clock skews
 input clk1, clk2 ;
+
 //Register Bank
 reg [31:0] RegBank [31:0] ; //Register bank ( 32 X 32 )
 
-//Memory - 1024 x 32
+//Memory Unit - 1024 x 32
 reg [31:0] Mem [1023:0] ;
 
 //Declaring the opcodes for the various instructions
@@ -22,14 +33,25 @@ parameter ADD = 6'b000000 , SUB = 6'b000001, AND = 6'b000010,
 parameter RR_ALU = 3'b000, RM_ALU = 3'b001, LOAD = 3'b010,
 		STORE = 3'b011, BRANCH = 3'b100, HALT = 3'b101 ;
 
+//RR_ALU - Register to Register ALU Instructions
+//RM_ALU - Register to Memory ALU Instructions 
+//LOAD - Load Type Instruction
+//STORE - Store Type Instruction
+//BRANCH - Branch Instruction 
+//HALT - Halt Instruction 
+
 reg [2:0] ID_EX_type, EX_MEM_type, MEM_WB_type ;
 
+
+
 reg HALTED ;
-//Set after HLT instruction is completed - in WB stage
+//Set after HLT instruction is completed - in Write Back stage
 
 reg TAKEN_BRANCH ;
-//Required to disable instructions after branch
+//Required to disable instructions after branching is done
 
+
+//---------- Defining Intermediate Registers Between Pipelines -----------//
 
 //Defining intermediate latches between Instruction Fetch stage
 //and Instruction Decode stage
@@ -39,50 +61,64 @@ reg [31:0] PC, IF_ID_IR, IF_ID_NPC ;
 //IR - Instruction Register
 //NPC - Next Program Counter
 
+
 //Defining intermediate latches between Instruction Decode stage and
 //Execute stage
 reg [31:0] ID_EX_IR, ID_EX_NPC, ID_EX_A, ID_EX_B, ID_EX_Imm ;
+
 
 //Defining intermediate latches between Execute Stage and Memory Stage
 reg [31:0] EX_MEM_IR, EX_MEM_ALUOut, EX_MEM_B ;
 reg EX_MEM_cond ;
 
+
 //Defining intermediate latches between Memory Stage and Write back stage
 reg [31:0] MEM_WB_IR, MEM_WB_ALUOut, MEM_WB_LMD ;
 
+///////////---------------------------------------------------//////////////////
+
+//-------------------- Stage 1 -----------------------------------//
 //Instruction Fetch Stage --- Stage 1
 
 always @ ( posedge clk1 )
 
-	if ( HALTED == 0 )
+	if ( HALTED == 0 ) //That is, instructions are still left to be executed
 		begin
 			if(((EX_MEM_IR[31:26] == BEQZ) && (EX_MEM_cond == 1)) || ((EX_MEM_IR[31:26] == BNEQZ) && (EX_MEM_cond == 0)))
+				//For branching Instructions 
 				begin
 					IF_ID_IR <= Mem[EX_MEM_ALUOut] ;
 					TAKEN_BRANCH <= 1'b1 ;
 					IF_ID_NPC <= EX_MEM_ALUOut + 1 ;
 					PC <= EX_MEM_ALUOut + 1 ;
 				end
-		else
-			begin
-				IF_ID_IR <= Mem[PC] ;
-				IF_ID_NPC <= PC + 1 ;
-				PC <= PC + 1 ;
-			end
+			else
+				//For Non Branching Instructions 
+				begin
+					IF_ID_IR <= Mem[PC] ;
+					IF_ID_NPC <= PC + 1 ;
+					PC <= PC + 1 ;
+				end
 		end
 
+
+///////-------End of Stage 1 ------------------------------------//
+
+
+
+//-------------------- Stage 2 -----------------------------------//
 //Instruction Decode Stage or Register Fetch stage --- Stage 2
 
 always @ ( posedge clk2 )
 
-	if(HALTED == 0 )
+	if(HALTED == 0 ) //That is, if there are instructions left to execute
 		begin
-			if(IF_ID_IR[25:21] == 5'b00000)
+			if(IF_ID_IR[25:21] == 5'b00000) //Register 0 - which is always at Ground
 				ID_EX_A <= 0 ;
 			else
-				ID_EX_A <= RegBank[IF_ID_IR[25:21]] ; //"rs"
+				ID_EX_A <= RegBank[IF_ID_IR[25:21]] ; //"rs" 
 
-			if(IF_ID_IR[20:16] == 5'b00000)
+			if(IF_ID_IR[20:16] == 5'b00000) //Register 0 - which is always at Ground 
 				ID_EX_B <= 0 ;
 			else
 				ID_EX_B <= RegBank[IF_ID_IR[20:16]] ; //"rt"
@@ -104,6 +140,11 @@ always @ ( posedge clk2 )
 		endcase
 	end
 
+//----------------------End of Stage 2 -------------------------------////////
+
+
+
+//-------------------------- Stage 3 ----------------------------------------////
 //Execution Stage or Effective Address Calculation --- Stage 3
 
 always @ (posedge clk1)
